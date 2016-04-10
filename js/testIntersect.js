c08f16e22bd7e99d45ff6f5f5bc46fef014562d2
Moni27b <moni27b@hotmail.com>
2016-04-10 13:03:20 +0200

    Version 1.4
    Nouveau Svg
    Image Semi-responsive

--- js/testIntersect.js
+++ js/testIntersect.js
@@ -2,12 +2,12 @@ $drawing.click(function(event) {
 	var shapeRef = new Path(event.target);    // Récupération de l'Object 2D de la forme cliquée
 	for (var id = 0; id < $("#drawing path").length; id++) {
 		var o = $("#drawing path")[id];
-		// La ligne suivante pue du cul comme on dit dans le jargon à cause des couleurs déjà présentes
+		
 		if (o != event.target && o.style && $("#select-color")[0].style && o.style.fill == $("#select-color")[0].style['background-color']) {
 			// Récupération de l'Object 2D de la forme i de la figure
 			var shapeI = new Path(o);
 			//On vérifie si on a une intersection
-			//pomme = Intersection.intersectShapes(shapeRef, shapeI);
+			
 			if (Intersection.intersectShapes(shapeRef, shapeI).points.length > 0) {
 				alert('Un voisin est d\351j\340 de la m\352me couleur.\nVeuillez choisir une couleur diff\351rente.');
 				return false;
